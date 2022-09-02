bc509c601a7fe009af442a92d0dad7b90667aea3
Tate-T <tatjana.tarasovych@gmail.com>
2022-09-02 17:12:26 +0300

    AddDebtPage layout

--- src/pages/AddDebtPage/AddDebtPage.jsx
+++ src/pages/AddDebtPage/AddDebtPage.jsx
@@ -1,13 +1,47 @@
 // import { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import s from './AddDebtPage.module.scss';
 
 export default function AddDebtPage() {
   return (
     <div>
-      <NavLink exact to="/" className="navLink" activeClassName="activeLink">
-        Go Back
+      <NavLink exact to="/" className={s.navLink} activeClassName="activeLink">
+        &#8701; Go Back
       </NavLink>
-      <h1>Add a Debt</h1>
+      <h2 className={s.mainTitle}>Add a Debt</h2>
+      <form className={s.form}>
+        <h3 className={s.title}>Borrower</h3>
+        <label htmlFor="">
+          {' '}
+          Borrower name
+          <input type="text" placeholder="Enter name" />
+        </label>
+        <label htmlFor="">
+          Borrower phone
+          <input type="text" placeholder="Enter phone number" />
+        </label>
+
+        <h3 className={s.title}>Debt</h3>
+        <label htmlFor="">
+          Debt name
+          <input type="text" placeholder="Enter name" />
+        </label>
+        <label htmlFor="">
+          Outstanding amount
+          <input type="text" placeholder="Enter amount" />
+        </label>
+        <label htmlFor="">
+          Interest rate
+          <input type="text" placeholder="Enter" />
+        </label>
+        <label htmlFor="">
+          Minimal payment
+          <input type="text" placeholder="Enter amount" />
+        </label>
+        <button type="sunbmit" className={s.btn}>
+          Add debt
+        </button>
+      </form>
     </div>
   );
 }
