1be84d2709e72585e8c5b0cfb29ee9abb4f7883a
Tate-T <tatjana.tarasovych@gmail.com>
2022-09-03 00:57:00 +0300

    notiflix Notify message btn

--- src/components/DebtsList/DebtsList.jsx
+++ src/components/DebtsList/DebtsList.jsx
@@ -1,26 +1,43 @@
-import { NavLink } from 'react-router-dom';
 import { ReactComponent as Bell } from '../../images/bell.svg';
 import { ReactComponent as Plus } from '../../images/plus.svg';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import s from './DebtsList.module.scss';
 
-const DebtsList = ({ debts, isOpenModal }) => {
+const DebtsList = ({
+  debts,
+  isOpenModal,
+  borrowerName,
+  minPayment,
+  amount,
+  name,
+}) => {
+  const notifyMessage = () => {
+    const message = Notify.warning(`Hey ${borrowerName}! 
+
+    You have an outstanding amount of ${amount} for ${name}.
+    
+    Please don't forget to pay your minimum monthly payment of ${minPayment}/month.`);
+
+    return message;
+  };
+
   return (
     <ul className={s.debtsList}>
       {debts.map(debt => (
         <li key={debt.id} className={s.debtCard}>
           <div className={s.titleBtnDirection}>
             <h2 className={s.debtTitle}>{debts.title}</h2>
-            <button type="button" className={s.debtBtn}>
+            <button
+              type="button"
+              className={s.debtBtn}
+              onClick={notifyMessage()}
+            >
               <Plus className={s.bell} /> Extra payment
             </button>
-            <NavLink
-              to="/notify"
-              className={s.debtBtn}
-              activeClassName="activeLink"
-            >
+            <button className={s.debtBtn}>
               <Bell className={s.bell} />
               Notify
-            </NavLink>
+            </button>
           </div>
           <h3 className={s.debterName}>{debts.name}</h3>
           <p className={s.debtDescr}>
@@ -42,14 +59,10 @@ const DebtsList = ({ debts, isOpenModal }) => {
           <button type="button" className={s.debtBtn}>
             <Plus className={s.bell} /> Extra payment
           </button>
-          <NavLink
-            to="/notify"
-            className={s.debtBtn}
-            activeClassName="activeLink"
-          >
+          <button className={s.debtBtn} onClick={notifyMessage}>
             <Bell className={s.bell} />
             Notify
-          </NavLink>
+          </button>
         </div>
         <h3 className={s.debterName}>John Doe</h3>
         <p className={s.debtDescr}>
@@ -68,14 +81,10 @@ const DebtsList = ({ debts, isOpenModal }) => {
           <button type="button" className={s.debtBtn}>
             <Plus className={s.bell} /> Extra payment
           </button>
-          <NavLink
-            to="/notify"
-            className={s.debtBtn}
-            activeClassName="activeLink"
-          >
+          <button className={s.debtBtn} onClick={notifyMessage}>
             <Bell className={s.bell} />
             Notify
-          </NavLink>
+          </button>
         </div>
         <h3 className={s.debterName}>John Doe</h3>
         <p className={s.debtDescr}>
@@ -94,14 +103,10 @@ const DebtsList = ({ debts, isOpenModal }) => {
           <button type="button" className={s.debtBtn}>
             <Plus className={s.bell} /> Extra payment
           </button>
-          <NavLink
-            to="/notify"
-            className={s.debtBtn}
-            activeClassName="activeLink"
-          >
+          <button className={s.debtBtn} onClick={notifyMessage}>
             <Bell className={s.bell} />
             Notify
-          </NavLink>
+          </button>
         </div>
         <h3 className={s.debterName}>John Doe</h3>
         <p className={s.debtDescr}>
