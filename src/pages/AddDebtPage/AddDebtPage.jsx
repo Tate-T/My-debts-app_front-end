56518463dab50433996f92c1cd01cc6b66af4a38
Tate-T <tatjana.tarasovych@gmail.com>
2022-09-02 22:37:33 +0300

    form margins

--- src/pages/AddDebtPage/AddDebtPage.jsx
+++ src/pages/AddDebtPage/AddDebtPage.jsx
@@ -9,43 +9,46 @@ export default function AddDebtPage() {
         &#8701; Go Back
       </NavLink>
       <h2 className={s.mainTitle}>Add a Debt</h2>
-      <form className={s.form}>
-        <h3 className={s.title}>Borrower</h3>
-        <label htmlFor="" className={s.label}>
-          {' '}
-          Borrower name <br />
-          <input type="text" placeholder="Enter name" className={s.input} />
-        </label>
-        <br />
-        <label htmlFor="" className={s.label}>
-          Borrower phone <br />
-          <input
-            type="text"
-            placeholder="Enter phone number"
-            className={s.input}
-          />
-        </label>{' '}
-        <br />
-        <h3 className={s.title}>Debt</h3>
-        <label htmlFor="" className={s.label}>
-          Debt name <br />
-          <input type="text" placeholder="Enter name" className={s.input} />
-        </label>{' '}
-        <br />
-        <label htmlFor="" className={s.label}>
-          Outstanding amount <br />
-          <input type="text" placeholder="Enter amount" className={s.input} />
-        </label>{' '}
-        <br />
-        <label htmlFor="" className={s.label}>
-          Interest rate <br />
-          <input type="text" placeholder="Enter" className={s.input} />
-        </label>{' '}
-        <br />
-        <label htmlFor="" className={s.label}>
-          Minimal payment <br />
-          <input type="text" placeholder="Enter amount" className={s.input} />
-        </label>
+      <form className={s.formContainer}>
+        <div className={s.form}>
+          <h3 className={s.title}>Borrower</h3>
+          <label htmlFor="" className={s.label}>
+            {' '}
+            Borrower name <br />
+            <input type="text" placeholder="Enter name" className={s.input} />
+          </label>
+          <br />
+          <label htmlFor="" className={s.label}>
+            Borrower phone <br />
+            <input
+              type="text"
+              placeholder="Enter phone number"
+              className={s.input}
+            />
+          </label>{' '}
+        </div>
+        <div className={s.form}>
+          <h3 className={s.title}>Debt</h3>
+          <label htmlFor="" className={s.label}>
+            Debt name <br />
+            <input type="text" placeholder="Enter name" className={s.input} />
+          </label>{' '}
+          <br />
+          <label htmlFor="" className={s.label}>
+            Outstanding amount <br />
+            <input type="text" placeholder="Enter amount" className={s.input} />
+          </label>{' '}
+          <br />
+          <label htmlFor="" className={s.label}>
+            Interest rate <br />
+            <input type="text" placeholder="Enter" className={s.input} />
+          </label>{' '}
+          <br />
+          <label htmlFor="" className={s.label}>
+            Minimal payment <br />
+            <input type="text" placeholder="Enter amount" className={s.input} />
+          </label>
+        </div>
         <button type="sunbmit" className={s.btn}>
           Add debt
         </button>
